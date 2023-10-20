74b5615d77e54722bf2bc077a9d319bbbed5b1ae
koberidzemikheili <mikheilikoberidze1@gmail.com>
2023-10-20 18:57:37 +0400

    feat: add input tasks, change the way operations work

--- src/TodoList.vue
+++ src/TodoList.vue
@@ -1,54 +1,69 @@
 <script setup>
 import TodoItem from './components/TodoItem.vue'
-import { ref } from 'vue'
+import PlusIcon from '@/components/icons/PlusIcon.vue'
+import { onMounted, ref } from 'vue'
 import axios from 'axios'
 
 const responseData = ref(null)
+const taskInput = ref('')
 
-const getData = async () => {
+onMounted(async () => {
   try {
     const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
     responseData.value = response.data
   } catch (error) {
     console.error('Error:', error)
   }
-}
+})
 
-const handleEditTodo = (newTitle, todoId) => {
-  const updatedResponseData = responseData.value.map((item) => {
-    if (item.id === todoId) {
-      return { ...item, title: newTitle }
-    }
-    return item
-  })
-
+const handleEditTodo = (newTitle, todoIndex) => {
+  const updatedResponseData = [...responseData.value]
+  updatedResponseData[todoIndex] = { ...updatedResponseData[todoIndex], title: newTitle }
   responseData.value = updatedResponseData
 }
 
-const handleDeleteTodo = (todoId) => {
-  responseData.value = responseData.value.filter((item) => item.id !== todoId)
-}
-const handleCheckTodo = (todoId, isCompleted) => {
-  const updatedResponseData = responseData.value.map((item) => {
-    if (item.id === todoId) {
-      return { ...item, completed: isCompleted }
-    }
-    return item
-  })
-
+const handleDeleteTodo = (todoIndex) => {
+  const updatedResponseData = [...responseData.value]
+  updatedResponseData.splice(todoIndex, 1)
   responseData.value = updatedResponseData
+}
+
+const handleCheckTodo = (todoIndex, isCompleted) => {
+  const updatedResponseData = [...responseData.value]
+  updatedResponseData[todoIndex] = { ...updatedResponseData[todoIndex], completed: isCompleted }
+  responseData.value = updatedResponseData
+}
+
+const AddTodo = () => {
+  if (taskInput.value.trim() !== '') {
+    const newTask = {
+      title: taskInput.value,
+      completed: false
+    }
+    responseData.value.unshift(newTask)
+    taskInput.value = ''
+  }
 }
 </script>
 
 <template>
   <main>
-    <button @click="getData">Fetch Data</button>
     <div v-if="responseData">
-      <h2>Todo Tasks:</h2>
+      <div class="headerdiv">
+        <h2 style="white-space: nowrap">Todo Tasks:</h2>
+        <form class="newtaskdiv" @submit.prevent="AddTodo">
+          <label class="labelstyles">New Task</label>
+          <input class="inputstyles" v-model="taskInput" />
+          <button class="plusbutton">
+            <PlusIcon class="svgsizes"></PlusIcon>
+          </button>
+        </form>
+      </div>
       <TodoItem
-        v-for="item in responseData"
-        :key="item.id"
+        v-for="(item, index) in responseData"
+        :key="index"
         :todo="item"
+        :todoid="index"
         @edit-todo="handleEditTodo"
         @delete-todo="handleDeleteTodo"
         @check-todo="handleCheckTodo"
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -4,17 +4,19 @@ import DeleteIcon from '@/components/icons/DeleteIcon.vue'
 import { ref, watch } from 'vue'
 
 const props = defineProps({
-  todo: Object
+  todo: Object,
+  todoid: Number
 })
 
 const emit = defineEmits(['edit-todo', 'delete-todo', 'check-todo'])
 
 const editing = ref(false)
-const editedText = ref(props.todo.title)
+const editedText = ref('')
 const isCompleted = ref(props.todo.completed)
 
 const startEditing = () => {
   editing.value = editing.value ? false : true
+  editedText.value = props.todo.title
 }
 
 const stopEditing = () => {
@@ -23,20 +25,23 @@ const stopEditing = () => {
 
 const saveTitle = () => {
   editing.value = false
-  emit('edit-todo', editedText.value, props.todo.id)
+  emit('edit-todo', editedText.value, props.todoid)
 }
 
 const deleteItem = () => {
-  emit('delete-todo', props.todo.id)
+  emit('delete-todo', props.todoid)
 }
 
 watch(isCompleted, (newValue) => {
-  emit('check-todo', props.todo.id, newValue)
+  emit('check-todo', props.todoid, newValue)
 })
 </script>
 
 <template>
   <div class="itemstyle">
+    <label>
+      <input class="checkboxstyles" type="checkbox" v-model="isCompleted" />
+    </label>
     <p @dblclick="startEditing" :class="{ 'completed-task': isCompleted }">
       {{ editing ? '' : todo.title }}
     </p>
@@ -47,9 +52,6 @@ watch(isCompleted, (newValue) => {
       @blur="stopEditing"
       @keydown.enter="saveTitle"
     />
-    <label>
-      <input class="checkboxstyles" type="checkbox" v-model="isCompleted" />
-    </label>
     <EditIcon class="svgsize editbutton" @click="startEditing"></EditIcon>
     <DeleteIcon class="svgsize editbutton" @click="deleteItem"></DeleteIcon>
   </div>
